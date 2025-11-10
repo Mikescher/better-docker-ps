Mikescher/better-docker-ps

commit cad5819c83578e80a17b43db00a9cb60a7610d5d

A few minor changes on PR #15
(cherry picked from commit 92f98aaee33ed5bc764065caf926d9e00ee19a54)

cli/options.go
@@ -1,12 +1,11 @@
 package cli
 
 import (
-	"bufio"
 	"encoding/json"
-	"os/exec"
 	"regexp"
 	"time"
 
+	"git.blackforestbytes.com/BlackForestBytes/goext/cmdext"
 	"git.blackforestbytes.com/BlackForestBytes/goext/termext"
 )
 
@@ -43,7 +42,6 @@ type Options struct {
 }
 
 func DefaultCLIOptions() Options {
-	socket := activeDockerContextSocket()
 	return Options{
 		Version:          false,
 		Help:             false,
@@ -53,7 +51,7 @@ func DefaultCLIOptions() Options {
 		TimeZone:         time.Local,
 		TimeFormatHeader: "Z07:00 MST",
 		TimeFormat:       "2006-01-02 15:04:05",
-		Socket:           socket,
+		Socket:           "auto",
 		Input:            nil,
 		All:              false,
 		WithSize:         false,
@@ -84,38 +82,30 @@ func DefaultCLIOptions() Options {
 	}
 }
 
-const defaultSocket = "/var/run/docker.sock"
+func (o Options) GetSocket() string {
+	const defaultSocket = "/var/run/docker.sock"
 
-// Get the socket for the active docker context
-//
-// This uses `docker context list --format json` and looks for the line with `"Current": true`.
-//
-// If it errors at any point it just returns `defaultSocket`.
-func activeDockerContextSocket() string {
-	cmd := exec.Command("docker", "context", "list", "--format", "json")
-	stdout, err := cmd.StdoutPipe()
+	if o.Socket != "auto" {
+		return o.Socket
+	}
+
+	res, err := cmdext.Runner("docker").Arg("context").Arg("list").Arg("--format").Arg("json").Timeout(10 * time.Second).FailOnTimeout().FailOnExitCode().Run()
 	if err != nil {
+		// on error we just return the default socket
 		return defaultSocket
 	}
 
-	err = cmd.Start()
+	var context dockerContext
+	err = json.Unmarshal([]byte(res.StdOut), &context)
 	if err != nil {
+		// on error we just return the default socket
 		return defaultSocket
 	}
-
-	// json format is actually jsonlines, so read line-by-line until you find the active one
-	scanner := bufio.NewScanner(stdout)
-	for scanner.Scan() {
-		var context dockerContext
-		err = json.Unmarshal(scanner.Bytes(), &context)
-		if err != nil {
-			continue
-		}
-		if context.Current {
-			return context.socket()
-		}
+	if context.Current {
+		return context.socket()
 	}
 
+	// if we don't have a current context, we just return the default socket
 	return defaultSocket
 }
 

cmd/dops/main.go
@@ -6,9 +6,10 @@ import (
 	"better-docker-ps/impl"
 	"better-docker-ps/pserr"
 	"fmt"
-	"git.blackforestbytes.com/BlackForestBytes/goext/langext"
 	"os"
 	"runtime/debug"
+
+	"git.blackforestbytes.com/BlackForestBytes/goext/langext"
 )
 
 // Inspiration: https://github.com/moby/moby/issues/7477
@@ -99,11 +100,13 @@ func printHelp(ctx *cli.PSContext) {
 	ctx.PrintPrimaryOutput("  --timezone                         Specify the timezone for date outputs")
 	ctx.PrintPrimaryOutput("  --color <true|false>               Enable/Disable terminal color output")
 	ctx.PrintPrimaryOutput("  --no-color                         Disable terminal color output")
-	ctx.PrintPrimaryOutput("  --socket <filepath>                Specify the docker socket location (Default: /var/run/docker.sock)")
+	ctx.PrintPrimaryOutput("  --socket <filepath>                Specify the docker socket location (Default: `auto` - which calls the docker cli to determine the socket)")
 	ctx.PrintPrimaryOutput("  --timeformat <go-time-fmt>         Specify the datetime output format (golang syntax)")
 	ctx.PrintPrimaryOutput("  --no-header                        Do not print the table header")
 	ctx.PrintPrimaryOutput("  --simple-header                    Do not print the lines under the header")
 	ctx.PrintPrimaryOutput("  --format <fmt>                     You can specify multiple formats and the first one that fits your terminal widt will be used")
+	ctx.PrintPrimaryOutput("  --sort <col>                       Sort output by a specific column, use the same identifier as in --format, only useful together with table formats ")
+	ctx.PrintPrimaryOutput("  --sort-direction <ASC|DESC>        The sort direction, only useful in combination with --sort")
 	ctx.PrintPrimaryOutput("  --watch <interval>                 Automatically refresh output periodically (interval is optional, default: 2s)")
 	ctx.PrintPrimaryOutput("")
 	ctx.PrintPrimaryOutput("Available --format keys (default):")

docker/api.go
@@ -27,10 +27,12 @@ func ListContainer(ctx *cli.PSContext) ([]byte, error) {
 		return data, nil
 	}
 
+	socket := ctx.Opt.GetSocket()
+
 	client := http.Client{
 		Transport: &http.Transport{
 			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
-				return net.Dial("unix", ctx.Opt.Socket)
+				return net.Dial("unix", socket)
 			},
 		},
 	}

commit b13200cd71f871e38f0ccd9548fc92412fa5cbaf

fix: auto socket recognition w/ multiple contexts
This fixes an issue introduced in cad5819 that broke the automatic socket
determination using docker contexts (originally introduced in
https://github.com/Mikescher/better-docker-ps/pull/15). Since the command
`docker context list --format json` outputs jsonlines format, if you have
more than 1 context, it would fail to unmarshal and then return the
default socket, effectively making the feature useless.

cli/options.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"encoding/json"
 	"regexp"
+	"strings"
 	"time"
 
 	"git.blackforestbytes.com/BlackForestBytes/goext/cmdext"
@@ -95,14 +96,16 @@ func (o Options) GetSocket() string {
 		return defaultSocket
 	}
 
-	var context dockerContext
-	err = json.Unmarshal([]byte(res.StdOut), &context)
-	if err != nil {
-		// on error we just return the default socket
-		return defaultSocket
-	}
-	if context.Current {
-		return context.socket()
+	// json format is actually jsonlines, so read line-by-line until you find the active one
+	for _, line := range strings.Split(res.StdOut, "\n") {
+		var context dockerContext
+		err = json.Unmarshal([]byte(line), &context)
+		if err != nil {
+			continue
+		}
+		if context.Current {
+			return context.socket()
+		}
 	}
 
 	// if we don't have a current context, we just return the default socket